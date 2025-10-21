8d3fe7cb5a3740abb3bdd754d125ca9e7c1e64ce
nhatminhdo-217 <nhatminh21072003@gmail.com>
2025-10-21 15:23:43 +0700

    create syntax_statement folder

---
+++ syntax_statement/App.java
@@ -0,0 +1,11 @@
+package syntax_statement;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("This is a statement");
+        System.out.println("This is a statement2");
+        System.out.println("This is a statement3");
+        System.out.println("This is a statement4");
+        System.out.println("This is a statement5");
+    }
+}
