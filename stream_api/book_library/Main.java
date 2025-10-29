9585fd751b419512029f7c2798732fb218b18dfc
nhatminhdo-217 <nhatminh21072003@gmail.com>
2025-10-29 15:30:09 +0700

    practice with stream api

--- stream_api/book_library/Main.java
+++ stream_api/book_library/Main.java
@@ -2,33 +2,70 @@ package stream_api.book_library;
 
 import java.time.LocalDate;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Main {
-    List<Book> library = Arrays.asList(
-            new Book(1, "The Great Gatsby", "F. Scott Fitzgerald", Genre.FICTION, LocalDate.of(1925, 12, 03), 10.99,
-                    180),
-            new Book(2, "1984", "George Orwell", Genre.FICTION, LocalDate.of(1949, 10, 12), 12.50, 328),
-            new Book(3, "Sapiens", "Yuval Noah Harari", Genre.NON_FICTION, LocalDate.of(2011, 06, 03), 15.75, 512),
-            new Book(4, "Dune", "Frank Herbert", Genre.SCIENCE_FICTION, LocalDate.of(1965, 04, 23), 14.99, 412),
-            new Book(5, "A Brief History of Time", "Stephen Hawking", Genre.NON_FICTION, LocalDate.of(1988, 06, 22),
-                    11.99, 256),
-            new Book(6, "The Hobbit", "J.R.R. Tolkien", Genre.FANTASY, LocalDate.of(1937, 04, 25), 9.99, 310),
-            new Book(7, "The Catcher in the Rye", "J.D. Salinger", Genre.FICTION, LocalDate.of(1951, 11, 23), 8.50,
-                    224),
-            new Book(8, "Foundation", "Isaac Asimov", Genre.SCIENCE_FICTION, LocalDate.of(1951, 01, 29), 13.00, 255));
+        List<Book> library = Arrays.asList(
+                        new Book(1, "The Great Gatsby", "F. Scott Fitzgerald", Genre.FICTION,
+                                        LocalDate.of(1925, 12, 03), 10.99,
+                                        180),
+                        new Book(2, "1984", "George Orwell", Genre.FICTION, LocalDate.of(1949, 10, 12), 12.50, 328),
+                        new Book(3, "Sapiens", "Yuval Noah Harari", Genre.NON_FICTION, LocalDate.of(2011, 06, 03),
+                                        15.75, 512),
+                        new Book(4, "Dune", "Frank Herbert", Genre.SCIENCE_FICTION, LocalDate.of(1965, 04, 23), 14.99,
+                                        412),
+                        new Book(5, "A Brief History of Time", "Stephen Hawking", Genre.NON_FICTION,
+                                        LocalDate.of(1988, 06, 22),
+                                        11.99, 256),
+                        new Book(6, "The Hobbit", "J.R.R. Tolkien", Genre.FANTASY, LocalDate.of(1937, 04, 25), 9.99,
+                                        310),
+                        new Book(7, "The Catcher in the Rye", "J.D. Salinger", Genre.FICTION,
+                                        LocalDate.of(1951, 11, 23), 8.50,
+                                        224),
+                        new Book(8, "Foundation", "Isaac Asimov", Genre.SCIENCE_FICTION, LocalDate.of(1951, 01, 29),
+                                        13.00, 255),
+                        new Book(9, "300 Bai Code Thieu Nhi", "Nhat Minh Do", Genre.SCIENCE_FICTION, LocalDate.now(),
+                                        69, 300),
+                        new Book(10, "Bach Tuyet va bay chu lun", "Nhat Minh Do", Genre.FANTASY, LocalDate.now(), 69,
+                                        250));
 
-    public static void main(String[] args) {
-        Main main = new Main();
+        public static void main(String[] args) {
+                Main main = new Main();
 
-        //Find all book published after 1950
-        // List<Book> after1950sBook = main.library.stream().filter(book -> book.getPublishDate().getYear() > 1950).collect(Collectors.toList());
-        // System.out.println("---------- Stream Result ----------");
-        // System.out.println("List book publish after 1950");
-        // after1950sBook.forEach(System.out::println);
+                // Find all book published after 1950
+                List<Book> after1950sBook = main.library.stream().filter(book ->
+                book.getPublishDate().getYear() > 1950).collect(Collectors.toList());
+                System.out.println("---------- Stream Result ----------");
+                System.out.println("List book publish after 1950");
+                after1950sBook.forEach(System.out::println);
 
-        //Get list of all book title
-        main.library.stream().map(book -> book.getTitle()).forEach(System.out::println);;
-    }
+                // Get list of all book title
+                main.library.stream().map(book ->
+                book.getTitle()).forEach(System.out::println);;
+
+                // Get all FICTION book and sort by name
+                List<Book> getAllFictionBookSortedByName = main.library.stream()
+                                .filter(book -> book.getGenre() == Genre.FICTION)
+                                .sorted(Comparator.comparing(Book::getTitle)).collect(Collectors.toList());
+                getAllFictionBookSortedByName.forEach(System.out::println);
+
+                //Get a Set of unique author name
+                main.library.stream().map(book -> book.getAuthor()).collect(Collectors.toSet()).forEach(System.out::println);
+
+                //Check if any book in the list is gerne MISTERY
+                boolean haveMisteryGenre = main.library.stream().anyMatch(book -> book.getGenre() == Genre.MYSTERY);
+                System.out.println(haveMisteryGenre);
+
+                //Find the most expesive book
+                Book maxPriceBook = main.library.stream().max(Comparator.comparing(Book::getPrice)).get();
+                System.out.println(maxPriceBook);
+
+                //Get a list of all book titles published before 1960, in alphabet order
+                main.library.stream().filter(book -> book.getPublishDate().getYear() < 1960)
+                .sorted(Comparator.comparing(Book::getTitle))
+                .map(book -> book.getTitle())
+                .forEach(System.out::println);
+        }
 }
